83a70631d6c05d5a0b2773755d0cfe148205f931
seni28 <ines.cabral--toucheboeuf@student.junia.com>
2024-04-21 17:02:34 +0200

    Major edits on every pages

--- js/custom.js
+++ js/custom.js
@@ -7,38 +7,57 @@ function getYear() {
 
 getYear();
 
+// Attendre que le document soit prêt avant d'initialiser Slick
+$(document).ready(function() {
+    $('.custom_slick_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        fade: true,
+        adaptiveHeight: true,
+        asNavFor: '.slick_slider_nav',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: false
+            }
+        }]
+    });
 
-$('.custom_slick_slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    fade: true,
-    adaptiveHeight: true,
-    asNavFor: '.slick_slider_nav',
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-            dots: false
-        }
-    }]
-})
-
-$('.owl-carousel .owl-theme').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.custom_slick_slider',
-    centerMode: false,
-    focusOnSelect: true,
-    variableWidth: true
+    $('.owl-carousel .owl-theme').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.custom_slick_slider',
+        centerMode: false,
+        focusOnSelect: true,
+        variableWidth: true
+    });
 });
 
+// Filtrage des éléments
+document.addEventListener("DOMContentLoaded", function() {
+    const filters = document.querySelectorAll("#furniture-filters li");
+    const items = document.querySelectorAll(".furn-item");
 
-/** google_map js **/
+    filters.forEach(function(filter) {
+        filter.addEventListener("click", function() {
+            const selectedFilter = this.getAttribute("data-filter");
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+            items.forEach(function(item) {
+                const itemFilter = item.getAttribute("data-filter");
+
+                if (selectedFilter === "*" || selectedFilter === itemFilter) {
+                    item.style.display = "block";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+
+            // Mettre à jour la classe active du filtre
+            filters.forEach(function(filter) {
+                filter.classList.remove("filter-active");
+            });
+            this.classList.add("filter-active");
+        });
+    });
+});
